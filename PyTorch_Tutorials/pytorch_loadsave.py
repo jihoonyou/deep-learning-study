4fb3e04f43e32d0b9691ec0f6938e52088edbc35
jihoon you <pianoetvoix@gmail.com>
2021-03-29 03:29:18 +0900

    Delete dummy files

--- PyTorch_Tutorials/pytorch_loadsave.py
+++ PyTorch_Tutorials/pytorch_loadsave.py
@@ -7,37 +7,52 @@ from torch.utils.data import DataLoader # easier data management
 import torchvision.datasets as datasets # pytorch standard dataset
 import torchvision.transforms as transforms # 
 
+# Create Fully Conneceted Network
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes): # MINST dataset 28x28 = 784
+        super(NN, self).__init__() # call parent's init
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
- # Set device
+class CNN(nn.Module):
+    def __init__(self, in_channels = 1, num_classes = 10):
+        super(CNN, self).__init__() # call parent's init
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1)) # keep the same dimension
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride = (2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+    
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+
+def save_checkpoint(state, filename='my_checkpoint.pth.tar'):
+    print('=> Saving checkpoint')
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print('=> Loading checkpoint')
+    model.load_state_dict(checkpoint['state_dict'])
+# Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
-input_size = 28
-sequence_length = 28
-num_layers = 2
-hidden_size = 256
+in_channel = 1
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64 # 한번에 학습시킬 data들 묶음
-num_epochs = 1
-
-# Create a bidirectional LSTM
-class BRNN(nn.Module):
-    def __init__(self, input, hidden_size, num_layers, num_classes):
-        super(BRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size*2, num_classes)
-
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
-        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
-
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
-
-        return out
+num_epochs = 5
 
 # Load Data
 train_dataset = datasets.MNIST(root='dataset/', train = True, transform=transforms.ToTensor(), download=True) # download data
@@ -47,7 +62,7 @@ test_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=T
 
 
 # Initialize network
-model = BRNN(input_size, hidden_size, num_layers, num_classes).to(device)
+model = CNN().to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -55,8 +70,12 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate) # 모델의 paramet
 
 # Train Netowrk
 for epoch in range(num_epochs):
+    
+    if epoch == 2:
+        checkpoint = { 'state_dict': model.state_dict(), 'optimzer': optimzer.state_dict()}
+        save_checkpoint(checkpoint)
     for batch, (data, targets) in enumerate(train_loader): # data is images, targets are correct digits for each image
-        data = data.to(device=device).squeeze(1)
+        data = data.to(device=device)
         targets = targets.to(device=device)
         # print(data.shape) # [64, 1, 28, 28] => [Num_of_images, black_or_white, size_of_images]
         
@@ -83,7 +102,7 @@ def check_accuracy(loader, model):
 
     with torch.no_grad(): #unnecessary computation / no gradient calculation
         for x, y in loader:
-            x = x.to(device=device).squeeze(1)
+            x = x.to(device=device)
             y = y.to(device=device)
 
             scores = model(x) # 64x10
